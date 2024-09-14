6a81fa9d99cddbe3e11b79656cbec448d7d10815
Sai Chaitanya Pachipulusu <chaitanya17.sai@gmail.com>
2024-09-14 02:52:48 -0400

    Updates

---
+++ precache-manifest.72eec819bef627f86e97011fdea57d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5be7b7581aa41644a7db154829c0f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b042cee512ee867525",
+    "url": "/static/css/main.4c30a4c4.chunk.css"
+  },
+  {
+    "revision": "f8d4c8236b0ff221dc7d",
+    "url": "/static/js/2.5aed70ca.chunk.js"
+  },
+  {
+    "revision": "0912cd81f878580995e7b503ed94c770",
+    "url": "/static/js/2.5aed70ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43219184ac4d571cbd02",
+    "url": "/static/js/3.42a1319c.chunk.js"
+  },
+  {
+    "revision": "40b042cee512ee867525",
+    "url": "/static/js/main.bc2039b2.chunk.js"
+  },
+  {
+    "revision": "5309358d1ade9de545ea",
+    "url": "/static/js/runtime-main.5a7452a7.js"
+  }
+]);
